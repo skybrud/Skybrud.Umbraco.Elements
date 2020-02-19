b1dedb9a485f783fbd7d5ee2552c113b706e85fd
Anders Bjerner <abjerner@skybrud.dk>
2020-02-19 13:42:54 +0100

    Rewrote logic for handling data type configurations
    New implementation relies more on Umbraco logic - eg. the ToValueEditor method

--- src/Skybrud.Umbraco.Elements/Models/ContentTypes/SkybrudElementsDataType.cs
+++ src/Skybrud.Umbraco.Elements/Models/ContentTypes/SkybrudElementsDataType.cs
@@ -1,9 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Reflection;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Umbraco.Core.Models;
-using Umbraco.Core.PropertyEditors;
-using Umbraco.Web.PropertyEditors;
 
 namespace Skybrud.Umbraco.Elements.Models.ContentTypes {
 
@@ -23,40 +19,7 @@ namespace Skybrud.Umbraco.Elements.Models.ContentTypes {
         public SkybrudElementsDataType(IDataType dataType) {
             Alias = dataType.Editor.Alias;
             View = dataType.Editor.GetValueEditor().View;
-            Config = GetConfig(dataType.Configuration);
-        }
-
-        private object GetConfig(object obj) {
-
-            if (obj == null) return null;
-
-            if (obj is Dictionary<string, object> dictionary) return dictionary;
-
-            Dictionary<string, object> temp = new Dictionary<string, object>();
-
-            // Umbraco apparently sets the "idType" config option on-the-fly
-            if (obj is MediaPickerConfiguration) {
-                temp.Add("idType", "udi");
-            }
-
-            foreach (var property in obj.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public)) {
-
-                var cf = property.GetCustomAttribute<ConfigurationFieldAttribute>();
-
-                if (cf != null) {
-
-                    temp.Add(cf.Key, property.GetValue(obj, null));
-
-                } else {
-
-                    temp.Add(property.Name, property.GetValue(obj, null));
-
-                }
-
-            }
-
-            return temp;
-
+            Config = dataType.Editor.GetConfigurationEditor().ToValueEditor(dataType.Configuration);
         }
 
     }
